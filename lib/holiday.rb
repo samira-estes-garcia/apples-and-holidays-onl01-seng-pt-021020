6a0149086a030d4ba26704d56ab08fc6e8527ea8
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-20 20:49:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   
   holiday_hash[:season] = 
   {
-    :holiday_name => 
+    :holiday_name => supply_array
   }
   
   holiday_hash
